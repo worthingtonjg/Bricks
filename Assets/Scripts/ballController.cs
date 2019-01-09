2a0b36b286d905b5590563a4ff5d8aa5517cc9c8
Jonathan Wortington <worthingtonjg@gmail.com>
2019-01-08 20:07:55 -0700

    initial commit

---
+++ Assets/Scripts/ballController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballController : MonoBehaviour
+{
+    public Rigidbody2D ball;
+
+    public float maxSpeed = 25f;
+
+    public bool fireOnAwake = false;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(fireOnAwake)
+        {
+            ball.velocity = Random.onUnitSphere * 10f;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        ball.velocity = Vector3.ClampMagnitude(ball.velocity, maxSpeed);
+    }
+}
